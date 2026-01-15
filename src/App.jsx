167e54509f228ed35eefaf447328b3d52bcdc661
Monique Carvalho <moniquecarvalho654@gmail.com>
2026-01-15 18:31:09 -0300

    feat: configure frontend routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+import AppRoutes from './routes';
+
 function App() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">
-        School Transport Finance
-      </h1>
-    </main>
-  )
+  return <AppRoutes />;
 }
 
-export default App
+export default App;
